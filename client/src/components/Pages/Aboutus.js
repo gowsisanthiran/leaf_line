42df00a88e81a9776d8fa799aaa728dffe33e88e
gowsisanthiran <gowshisanthiran@gmail.com>
2023-05-12 10:27:04 +0530

    ui developed

--- client/src/components/Pages/Aboutus.js
+++ client/src/components/Pages/Aboutus.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import image from '/home/gowsi/Documents/Final_Project/client/src/images/20 Idli Varieties - Different Types Of Idli Recipes.jpeg'
 
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
+    <div>Aboutus <br></br>
+      <p>Biodegradable banana leaf plates are an eco-friendly alternative to plastic or paper plates. These plates are made from the leaves of the banana plant, which are both abundant and renewable. The leaves are collected, cleaned, and then molded into plates of different shapes and sizes.<br></br>
+
+One of the main benefits of using biodegradable banana leaf plates is that they are fully compostable. Unlike plastic or paper plates that can take hundreds of years to decompose, banana leaf plates break down in a matter of weeks. This means that they do not contribute to the pollution of landfills and oceans.
+<br></br>
+In addition to being eco-friendly, banana leaf plates are also aesthetically pleasing. The natural texture and color of the leaves give the plates a unique and rustic look that can enhance the presentation of food.
+
+Furthermore, using biodegradable banana leaf plates supports local communities and farmers. The production of these plates often involves small-scale farmers who earn a living by collecting the leaves and selling them to plate manufacturers.
+
+Overall, biodegradable banana leaf plates offer a sustainable and eco-friendly option for those who want to reduce their environmental impact while still enjoying the convenience of disposable plates.</p>
+
+<img src ='/home/gowsi/Documents/Final_Project/client/src/images/20 Idli Varieties - Different Types Of Idli Recipes.jpeg' alt='about'></img>
+
+
+    </div>
+    
   )
 }
 
