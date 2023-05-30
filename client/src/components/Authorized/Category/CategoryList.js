8d37d8b2a3bb1efb11a2513ef061f1313ff21479
shangayu <shangayu8@gmail.com>
2023-05-30 13:59:22 +0530

    Dashboard update

--- client/src/components/Authorized/Category/CategoryList.js
+++ client/src/components/Authorized/Category/CategoryList.js
@@ -1,96 +1,119 @@
-// import React,{useEffect} from 'react';
-// import { DataGrid, GridToolbar } from '@mui/x-data-grid';
-// import {useDispatch,useSelector} from 'react-redux';
-// import {deleteCategory, getCategories, resetMutationResult, selectAllCategories, selectCategoryMutationResult} from '../../../redux/features/categorySlice';
-// import {toast} from 'react-toastify';
-// import {Link} from 'react-router-dom';
 
-// import {Box, Typography,IconButton, Tooltip} from '@mui/material';
+
+// import React, { useEffect } from 'react';
+// import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { deleteCategory, getCategories, resetMutationResult, selectAllCategories, selectCategoryMutationResult } from '../../../redux/features/categorySlice';
+// import { toast } from 'react-toastify';
+// import { Link } from 'react-router-dom';
+
+// import { Box, Typography, IconButton, Tooltip } from '@mui/material';
 // import DeleteForeeverIcon from '@mui/icons-material/DeleteForever';
 // import EditIcon from '@mui/icons-material/Edit';
 // import BoxShadowLoader from '../../../components/Skeletons/BoxShadowLoader';
 
 // const CategoryList = () => {
-//     const dispatch=useDispatch();
-//     const {loading, categories}=useSelector(selectAllCategories);
-//     const {success} = useSelector(selectCategoryMutationResult);
+//   const dispatch = useDispatch();
+//   const { loading, categories } = useSelector(selectAllCategories);
+//   const { success } = useSelector(selectCategoryMutationResult);
 
-//     const deleteHandler=(id)=>{
-//         dispatch(deleteCategory({id,toast}));
+//   const deleteHandler = (id) => {
+//     dispatch(deleteCategory({ id, toast }));
+//   };
+
+//   const columns = [
+//     { field: 'title', headerName: 'Categories', headerClassName: 'gridHeader', flex: 1, minWidth: 170 },
+//     { field: 'description', headerName: 'Description', headerClassName: 'gridHeader', flex: 1.5, minWidth: 250 },
+//     {
+//       field: 'actions',
+//       headerName: 'Actions',
+//       headerClassName: 'gridHeader',
+//       flex: 0.5,
+//       minWidth: 80,
+//       type: 'number',
+//       sortable: false,
+//       renderCell: (params) => {
+//         return (
+//           <>
+//             <Link to={`/authorized/category/${params.getValue(params.id, 'id')}`}>
+//               <Tooltip title='Edit' placement='top'>
+//                 <EditIcon sx={{ width: '30px', height: '30px', color: '#1b5e20' }} />
+//               </Tooltip>
+//             </Link>
+
+//             <Tooltip title='Delete' placement='top'>
+//               <IconButton
+//                 color='error'
+//                 component='span'
+//                 onClick={() => deleteHandler(params.getValue(params.id, 'id'))}
+//               >
+//                 <DeleteForeeverIcon sx={{ width: '30px', height: '30px' }} />
+//               </IconButton>
+//             </Tooltip>
+//           </>
+//         );
+//       },
+//     },
+//   ];
+
+//   const rows = categories
+//     ? categories.map((category) => ({
+//         id: category._id,
+//         title: category.title,
+//         description: category.description,
+//       }))
+//     : [];
+
+//   useEffect(() => {
+//     if (success) {
+//       dispatch(resetMutationResult());
 //     }
+//     dispatch(getCategories({ toast }));
+//   }, [dispatch, success]);
 
-//     const columns=[
-//         {field:'title', headerName:'Categories', headerClassName:'gridHeader',flex:1,minWidth:170},
-//         {field:'description', headerName:'Description', headerClassName:'gridHeader',flex:1.5,minWidth:250},
-//         {
-//             field:'actions', 
-//             headerName:'Actions', 
-//             headerClassName:'gridHeader',
-//             flex:.5,
-//             minWidth:80,
-//             type:'number',
-//             sortable:false,
-//             renderCell:(params)=>{
-//                 return (
-//                     <>
-//                         <Link to={`/authorized/category/${params.getValue(params.id,'id')}`}>
-//                         <Tooltip title='Edit' placement='top'>
-//                             <EditIcon sx={{width:'30px', height:'30px', color:'#1b5e20'}} />
-//                         </Tooltip>
-//                         </Link>
-
-//                         <Tooltip title='Delete' placement='top'>
-//                             <IconButton color='error' 
-//                                         component='span' 
-//                                         onClick={()=>deleteHandler(params.getValue(params.id,'id'))}>
-//                                 <DeleteForeeverIcon sx={{width:'30px', height:'30px'}}/>
-//                             </IconButton>
-//                         </Tooltip>
-
-//                     </>
-//                 )
-//             }
-//         }
-//     ]
-//     const rows=[];
-//     categories && categories.forEach(category => {
-//         rows.push({
-//             id:category._id,
-//             title:category.title,
-//             description:category.description
-//         })
-//     });
-//     useEffect(() => {
-//         if(success){
-//             dispatch(resetMutationResult());
-//         } 
-//         dispatch(getCategories({toast}))
-//     }, [dispatch,success])
-    
 //   return (
-//     <Box style={{displya:'flex', flexDirection:'column', width:'100%', marginTop:'15px', textAlign:'center'}}>
-//         <Typography component='h1' color="#1b5e20" variant='h5'sx={{m:4}}>Full list of categories</Typography>
-//         {loading ? <BoxShadowLoader/>:
-//         <DataGrid rows={rows}
-//                     columns={columns}
-//                     components={{Toolbar:GridToolbar}}                    
-//                     autoHeight
-//         />
-//         }
+//     <Box
+//       sx={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         width: '85%',
+//         textAlign: 'center',
+//         boxShadow: '0 2px 4px #285430',
+//         borderRadius: '4px',
+//         backgroundColor: '#fff',
+//         padding: '55px',
+//         justifyContent:"center",
+//         marginLeft:"105px"
+//       }}
+//     >
+//       <Typography component='h1' color='#1b5e20'  marginBottom="20px" variant='h5' sx={{ m: 4 }}>
+//         Full list of categories
+//       </Typography>
+//       {loading ? (
+//         <BoxShadowLoader />
+//       ) : (
+//         <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }} autoHeight />
+//       )}
 //     </Box>
-//   )
-// }
+//   );
+// };
 
-// export default CategoryList
+// export default CategoryList;
 
 import React, { useEffect } from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteCategory, getCategories, resetMutationResult, selectAllCategories, selectCategoryMutationResult } from '../../../redux/features/categorySlice';
+import {
+  deleteCategory,
+  getCategories,
+  resetMutationResult,
+  selectAllCategories,
+  selectCategoryMutationResult,
+} from '../../../redux/features/categorySlice';
 import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 
-import { Box, Typography, IconButton, Tooltip } from '@mui/material';
+import { Box, Typography, IconButton, Tooltip, Container } from '@mui/material';
 import DeleteForeeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 import BoxShadowLoader from '../../../components/Skeletons/BoxShadowLoader';
@@ -155,30 +178,32 @@ const CategoryList = () => {
   }, [dispatch, success]);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '85%',
-        textAlign: 'center',
-        boxShadow: '0 2px 4px #285430',
-        borderRadius: '4px',
-        backgroundColor: '#fff',
-        padding: '55px',
-        justifyContent:"center",
-        marginLeft:"105px"
-      }}
-    >
-      <Typography component='h1' color='#1b5e20'  marginBottom="20px" variant='h5' sx={{ m: 4 }}>
-        Full list of categories
-      </Typography>
-      {loading ? (
-        <BoxShadowLoader />
-      ) : (
-        <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }} autoHeight />
-      )}
-    </Box>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          textAlign: 'center',
+          boxShadow: '0 2px 4px #285430',
+          borderRadius: '4px',
+          backgroundColor: '#fff',
+          padding: '55px',
+          marginBottom: '20px',
+        }}
+      >
+        <Typography component='h1' color='#1b5e20' variant='h5' sx={{ m: 4 }}>
+          Full list of categories
+        </Typography>
+        {loading ? (
+          <BoxShadowLoader />
+        ) : (
+          <DataGrid rows={rows} columns={columns} components={{ Toolbar: GridToolbar }} autoHeight />
+        )}
+      </Box>
+    </Container>
   );
 };
 
 export default CategoryList;
+
--- client/src/components/Routes/AuthorizedRoute.js
+++ client/src/components/Routes/AuthorizedRoute.js
@@ -1,6 +1,6 @@
 
 import { useSelector } from 'react-redux';
-import { useNavigate, Outlet, Navigate, Link } from 'react-router-dom';
+import { useNavigate, Outlet, Navigate, Link, NavLink } from 'react-router-dom';
 import { selectLoggedInUser } from '../../redux/features/authSlice';
 import jwtDecode from 'jwt-decode';
 import { Box, Typography } from '@mui/material';
@@ -8,9 +8,26 @@ import TreeMenu from './Menus/TreeMenu';
 import './Authorized.css';
 import DrawerTreeMenu from './Menus/DrawerTreeMenu';
 import AdminDashboard from '../Authorized/Dashboard/AdminDashboard';
+import AuthMenu from '../Layout/AuthMenu';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { selectCartItems } from '../../redux/features/cartSlice';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
 
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: 6,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
 
 const AuthorizedRoute = () => {
+    const { products } = useSelector(selectCartItems);
     const navigate = useNavigate();
     const { accessToken } = useSelector(selectLoggedInUser);
     let role;
@@ -21,20 +38,28 @@ const AuthorizedRoute = () => {
             <>
                 <Box
                     sx={{
-                        background: '#f9fbe7',
+                        backgroundColor: "#E9EDC9",
+                        transition: ".4s all",
+                        overflow: "visible",
+                        top: "0",
+                        left: "0",
+                        width: "100%",
+                        zIndex: "100",
+                        position: "fixed",
                         boxShadow: '2px 2px 10px #7b9c56',
                         display: 'flex',
                         color: '#fff',
                         p: 1,
-                        borderRadius: '8px',
-                        marginLeft: "20px",
-                        marginRight: "20px"
+                        // borderRadius: '8px',
+                        // marginLeft: "20px",
+                        // marginRight: "20px",
+                        // paddingTop:"5px"
                     }}
                 >
                     <Box className='mTreeMenu' sx={{ minWidth: '225px', mr: 1 }}>
                         <DrawerTreeMenu />
                     </Box>
-                    <Box sx={{ flexGrow: 1 ,paddingTop: '10px'}}>
+                    <Box sx={{ flexGrow: 1, paddingTop: '10px' }}>
                         <Link to="dashboard" className="link-no-underline">
                             <Typography
                                 component='div'
@@ -49,14 +74,34 @@ const AuthorizedRoute = () => {
                                 Dashboard
                             </Typography>
                         </Link>
+
+
+
+
+
                     </Box>
+                    <div className="d-flex align-items-center justify-content-end">
+                        <NavLink to="/product" className="nav-link" activeClassName="active" style={{ color: '#263A29' }}>
+                            <Inventory2Icon />
+                        </NavLink>
+                        <NavLink to="/cart" className="nav-link" activeClassName="active" style={{ color: '#263A29' }}>
+                            <StyledBadge badgeContent={products.length} color="secondary">
+                                <ShoppingCartIcon />
+                            </StyledBadge>
+
+                        </NavLink>
+
+                        <div className="auth-area text-left">
+                            <AuthMenu />
+                        </div>
+                    </div>
                 </Box>
                 <Box
                     sx={{
                         display: 'flex',
                         flexGrow: 1,
-                        marginTop: '16px',
-                        marginLeft: "20px",
+                        marginTop: '60px',
+                        // marginLeft: "20px",
                         // borderRadius: '8px',
                         overflow: 'hidden',
                     }}
@@ -64,18 +109,19 @@ const AuthorizedRoute = () => {
                     <Box
                         className='dTreeMenu'
                         sx={{
-                            backgroundImage: 'linear-gradient(to right, #143a0d, #c0dca5)',
+                            backgroundColor: '#263A29',
                             boxShadow: '2px 2px 15px #7b9c56',
                             minWidth: '240px',
-                            maxHeight: '100px',
+                            maxHeight: '100%',
                             mr: 1,
-                            borderRadius: '10px',
+                            
+                            // borderRadius: '10px',
                         }}
                     >
                         <TreeMenu />
                     </Box>
                     <Box sx={{ flexGrow: 1 }}>
-                        <Box sx={{ m: '0 auto', p: 1 }}>
+                        <Box sx={{ }}>
                             <Outlet />
                         </Box>
                     </Box>
