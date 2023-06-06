900397fba043bc0d81f8d5c6ce4973b29d12bc42
shangayu <shangayu8@gmail.com>
2023-06-06 09:59:49 +0530

    landing page changes

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -131,6 +131,8 @@ import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
 import { selectCartItems } from '../../redux/features/cartSlice';
+
+
 import '../Layout/Header.css';
 import logo from '../../images/LOGO.png';
 import logoo from '../../images/logoo.png';
@@ -162,78 +164,61 @@ const Header = () => {
   const { products } = useSelector(selectCartItems);
 
   return (
-    <div className={`container-fluid main ${scrolled ? 'scrolled' : ''}`}>
-      <nav className={`navbar navbar-expand navbar-dark navbar navbar navbar-default fixed-top ${scrolled ? 'navbar-transparent' : ''}`} style={{ position: 'fixed', marginTop: '-100px', backgroundColor: scrolled ? '#899D60' : '' }}>
-      <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+   
 
-      <div className="navbar-logo">
-        <a className="logo" href="http://chaitan.like-themes.com/">
-          <img
-            width="254"
-            height="258"
-            src={logoo} 
-            className="attachment-full size-full"
-            alt="Logo" 
-            loading="lazy"
-            //srcSet="http://chaitan.like-themes.com/wp-content/uploads/2018/06/logo_01.png 254w, http://chaitan.like-themes.com/wp-content/uploads/2018/06/logo_01-24x24.png 24w, http://chaitan.like-themes.com/wp-content/uploads/2018/06/logo_01-48x48.png 48w"
-            sizes="(max-width: 254px) 100vw, 254px"
-          />
-        </a>
-      </div>
-      
-        
-        {/* <Link to="/" className="navbar-brand">
-          <img src={logoo} alt="Logo" className="logo" style={{  width: '350px', marginLeft: '150px',marginTop:'20px',marginBottom:'-40px',textAlign: 'center' }} />
-        </Link > */}
-        <div className="collapse navbar-collapse text-center justify-content-center" id="navbarNav"style={{marginTop:'40px',marginBottom:'-40px'}}>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link " activeClassName="active" style={{ color: '#654E30' }}>
-                <HomeIcon /> Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/product" className="nav-link" activeClassName="active" style={{ color: '#654E30' }}>
-                Product
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/cart" className="nav-link" activeClassName="active" style={{ color: '#654E30' }}>
-                <StyledBadge badgeContent={products.length}>
-                  <ShoppingCartIcon />
-                </StyledBadge>
-                Cart
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className="d-flex align-items-center justify-content-end" style={{ marginTop: '80px',color:'#bdf890' }}>
-          <div className="auth-area text-left">
+   
+     <div className={`container-fluid main ${scrolled ? 'scrolled' : ''}`}>
+      <nav className={`navbar navbar-expand navbar-dark fixed-top ${scrolled ? 'navbar-transparent' : ''}`} style={{ backgroundColor: scrolled ? 'rgba(8, 57, 43, 0.4)' : '' }}>
+        <div className="container"style={{marginTop:'-70px',marginBottom:'-50px'}}>
+          <NavLink to="/" className="navbar-brand">
+            <img src={logoo} alt="Logo" className="logo" style={{ height: '200px' }} />
+          </NavLink>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto justify-content-center align-items-center "style={{marginRight:'20px'}}>
+              <li className="nav-item"style={{marginLeft:'100px'}}>
+                <NavLink to="/" className="nav-link" activeClassName="active" style={{ fontWeight: 'bold', fontFamily: 'Open Sans, sans-serif',color:'#BACC81' }}>
+                  <HomeIcon /> Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/product" className="nav-link" activeClassName="active" style={{ fontWeight: 'bold', fontFamily: 'Open Sans, sans-serif',color:'#BACC81'  }}>
+                  <Inventory2Icon/>Product
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/cart" className="nav-link" activeClassName="active" style={{ fontWeight: 'bold', fontFamily: 'Open Sans',color:'#BACC81'  }}>
+                  <StyledBadge badgeContent={products.length}>
+                    <ShoppingCartIcon />
+                  </StyledBadge>
+                  Cart
+                </NavLink>
+              </li>
+            </ul>
+            <div className="auth-area text-left">
             <AuthMenu />
           </div>
+          </div>
+          {/* <div className="d-flex align-items-center justify-content-end" style={{ marginTop: '80px',color:'#bdf890' }}>
+          
+        </div> */}
         </div>
-      </nav>
 
-<div id="myCarousel" class="carousel carousel-fade slide" data-ride="carousel" data-interval="3000">
+      </nav>
+   
+
+ <div id="myCarousel" class="carousel carousel-fade slide" data-ride="carousel" data-interval="3000">
   <div class="carousel-inner" role="listbox">
     <div class="item active background a zoom-effect">
       {/* <img src="your-image-url.jpg" alt="Image" /> */}
     </div>
    
   </div>
-</div>
+ </div>
 
-<div class="covertext zoom-effect">
+ <div class="covertext zoom-effect">
   <div class="col-lg-10" style={{ float: "none", margin: "0 auto", marginTop: '10rem', fontFamily: 'NunitoSans,Verdana', color: '#fff', border: '2px ' }}>
     <h1 class="title">BIO-DEGRADABLE PLATES FOR A</h1>
     <h1 class="title">CLEANER PLANET</h1>
@@ -241,11 +226,11 @@ const Header = () => {
   <div class="col-xs-12 explore">
     <a href="/product"><button type="button" class="btn btn-lg explorebtn">EXPLORE</button></a>
   </div>
+ </div>
+
+
 </div>
 
-
-
-    </div>
 
 
   );
--- client/src/components/Pages/Contactus.js
+++ client/src/components/Pages/Contactus.js
@@ -1,70 +1,159 @@
-// import React from 'react'
-// import { Box,Typography, TextField, Button } from '@mui/material';
-// import { useState } from "react";
+// // import React from 'react'
+// // import { Box,Typography, TextField, Button } from '@mui/material';
+// // import { useState } from "react";
 
-// // @mui material components
-// import Container from "@mui/material/Container";
-// import Grid from "@mui/material/Grid";
-// import Switch from "@mui/material/Switch";
+// // // @mui material components
+// // import Container from "@mui/material/Container";
+// // import Grid from "@mui/material/Grid";
+// // import Switch from "@mui/material/Switch";
+
+// // const Contactus = () => {
+// //   const [checked, setChecked] = useState(true);
+
+// //    const handleChecked = () => setChecked(!checked);
+// //   // const handleSubmit = (e) => {
+// //   //   e.preventDefault();}
+
+// //   return (
+    
+// //     <Box component="section" py={12 } data-aos="fade-up" style={{backgroundColor: '#DAE2B6',boxShadow: '0 0 10px rgba(0, 100, 0, 0.5)',marginTop:'50px'}}>
+      
+// //       <Container>
+// //         <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
+// //           <Typography  mb={1} sx={{fontFamily: 'Open Sans , sans-serif',fontSize: '1.5rem',fontWeight: 'bold' }}>
+// //             Contact <span style={{color: '#7eaf6c '}}>Us</span>
+// //           </Typography>
+// //         </Grid>
+// //         <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
+// //           <Box width="100%" component="form" method="post" autoComplete="off">
+// //             <Box p={3}>
+// //               <Grid container spacing={3}>
+// //                 <Grid item xs={12} md={6} sx={{fontFamily: 'Poppins, sans-serif',fontSize:'15px'}}>
+// //                   <TextField label="First Name" variant="standard" fullWidth  />
+// //                 </Grid>
+// //                 <Grid item xs={12} md={6}>
+// //                   <TextField label="Last Name" variant="standard" fullWidth  />
+// //                 </Grid>
+// //                 <Grid item xs={12}>
+// //                   <TextField label="Email Address" variant="standard" type="email" fullWidth />
+// //                 </Grid>
+// //                 <Grid item xs={12}>
+// //                   <TextField label="Your Message" variant="standard" multiline fullWidth rows={6} />
+// //                 </Grid>
+// //                 <Grid item xs={12} alignItems="center" ml={-1}>
+// //                   <Switch checked={checked} onChange={handleChecked} />
+// //                   <Typography
+// //                     variant="button"
+// //                     fontWeight="regular"
+// //                     color="text"
+// //                     ml={-1}
+// //                     sx={{ cursor: "pointer", userSelect: "none" ,fontFamily: 'Poppins, sans-serif'}}
+// //                     onClick={handleChecked}
+// //                   >
+// //                     &nbsp;&nbsp;I agree to the&nbsp;
+// //                   </Typography>
+// //                   <Typography
+// //                     component="a"
+// //                     href="#"
+// //                     variant="button"
+// //                     fontWeight="regular"
+// //                     color="#1F6415"
+// //                   >
+// //                     Terms and Conditions
+// //                   </Typography>
+// //                 </Grid>
+// //               </Grid>
+// //               <Grid container item justifyContent="center" xs={12} my={2}>
+// //                 <Button type="submit" variant="contained" color="success" fullWidth>
+// //                   Send Message
+// //                 </Button>
+// //               </Grid>
+// //             </Box>
+// //           </Box>
+// //         </Grid>
+// //       </Container>
+      
+// //     </Box>
+// //   //   <div className="container">
+// //   //   <div className="contact_main">
+// //   //     <h1 className="request_text">A Call Back</h1>
+// //   //     <form onSubmit={handleSubmit}>
+// //   //       <div className="form-group">
+// //   //         <input type="text" className="email-bt" placeholder="Name" name="Name" />
+// //   //       </div>
+// //   //       <div className="form-group">
+// //   //         <input type="text" className="email-bt" placeholder="Email" name="Email" />
+// //   //       </div>
+// //   //       <div className="form-group">
+// //   //         <input type="text" className="email-bt" placeholder="Phone Number" name="PhoneNumber" />
+// //   //       </div>
+// //   //       <div className="form-group">
+// //   //         <textarea className="massage-bt" placeholder="Message" rows="5" id="comment" name="Message"></textarea>
+// //   //       </div>
+// //   //       <button type="submit" className="send_btn">SEND</button>
+// //   //     </form>
+// //   //   </div>
+// //   // </div>
+// //   );
+// // }
+
+
+
+
+// // export default Contactus
+
+// import React, { useState } from 'react';
+// import { Box, Typography, TextField, Button } from '@mui/material';
+// import Container from '@mui/material/Container';
+// import Grid from '@mui/material/Grid';
+// import Switch from '@mui/material/Switch';
 
 // const Contactus = () => {
 //   const [checked, setChecked] = useState(true);
 
-//    const handleChecked = () => setChecked(!checked);
-//   // const handleSubmit = (e) => {
-//   //   e.preventDefault();}
+//   const handleChecked = () => setChecked(!checked);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Add form submission logic here
+//   };
 
 //   return (
-    
-//     <Box component="section" py={12 } data-aos="fade-up" style={{backgroundColor: '#DAE2B6',boxShadow: '0 0 10px rgba(0, 100, 0, 0.5)',marginTop:'50px'}}>
-      
+//     <Box component="section" py={12} data-aos="fade-up" style={{ backgroundColor: '#DAE2B6', boxShadow: '0 0 10px rgba(0, 100, 0, 0.5)', paddingTop: '50px',marginTop:'50px' }}>
 //       <Container>
 //         <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
-//           <Typography  mb={1} sx={{fontFamily: 'Open Sans , sans-serif',fontSize: '1.5rem',fontWeight: 'bold' }}>
-//             Contact <span style={{color: '#7eaf6c '}}>Us</span>
+//           <Typography mb={1} sx={{ fontFamily: 'Open Sans , sans-serif', fontSize: '1.5rem', fontWeight: 'bold' }}>
+//             Contact <span style={{ color: 'green' }}>Us</span>
 //           </Typography>
 //         </Grid>
-//         <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
-//           <Box width="100%" component="form" method="post" autoComplete="off">
+//         <Grid container item xs={12} lg={7} sx={{ mx: 'auto' }}>
+//           <Box width="100%" component="form" method="post" autoComplete="off" onSubmit={handleSubmit}>
 //             <Box p={3}>
 //               <Grid container spacing={3}>
-//                 <Grid item xs={12} md={6} sx={{fontFamily: 'Poppins, sans-serif',fontSize:'15px'}}>
-//                   <TextField label="First Name" variant="standard" fullWidth  />
+//                 <Grid item xs={12} md={6} sx={{ fontFamily: 'Poppins, sans-serif', fontSize: '15px' }}>
+//                   <TextField label="First Name" variant="standard" fullWidth required />
 //                 </Grid>
 //                 <Grid item xs={12} md={6}>
-//                   <TextField label="Last Name" variant="standard" fullWidth  />
+//                   <TextField label="Last Name" variant="standard" fullWidth required />
 //                 </Grid>
 //                 <Grid item xs={12}>
-//                   <TextField label="Email Address" variant="standard" type="email" fullWidth />
+//                   <TextField label="Email Address" variant="standard" type="email" fullWidth required />
 //                 </Grid>
 //                 <Grid item xs={12}>
-//                   <TextField label="Your Message" variant="standard" multiline fullWidth rows={6} />
+//                   <TextField label="Your Message" variant="standard" multiline fullWidth rows={6} required />
 //                 </Grid>
 //                 <Grid item xs={12} alignItems="center" ml={-1}>
 //                   <Switch checked={checked} onChange={handleChecked} />
-//                   <Typography
-//                     variant="button"
-//                     fontWeight="regular"
-//                     color="text"
-//                     ml={-1}
-//                     sx={{ cursor: "pointer", userSelect: "none" ,fontFamily: 'Poppins, sans-serif'}}
-//                     onClick={handleChecked}
-//                   >
+//                   <Typography variant="button" fontWeight="regular" color="text" ml={-1} sx={{ cursor: 'pointer', userSelect: 'none', fontFamily: 'Poppins, sans-serif' }} onClick={handleChecked}>
 //                     &nbsp;&nbsp;I agree to the&nbsp;
 //                   </Typography>
-//                   <Typography
-//                     component="a"
-//                     href="#"
-//                     variant="button"
-//                     fontWeight="regular"
-//                     color="#1F6415"
-//                   >
+//                   <Typography component="a" href="#" variant="button" fontWeight="regular" color="#1F6415">
 //                     Terms and Conditions
 //                   </Typography>
 //                 </Grid>
 //               </Grid>
 //               <Grid container item justifyContent="center" xs={12} my={2}>
-//                 <Button type="submit" variant="contained" color="success" fullWidth>
+//                 <Button type="submit" variant="contained" color="success" fullWidth style={{color:"#D2FBA4",backgroundColor: '#1A2902',borderRadius:'15px'}} >
 //                   Send Message
 //                 </Button>
 //               </Grid>
@@ -72,97 +161,86 @@
 //           </Box>
 //         </Grid>
 //       </Container>
-      
 //     </Box>
-//   //   <div className="container">
-//   //   <div className="contact_main">
-//   //     <h1 className="request_text">A Call Back</h1>
-//   //     <form onSubmit={handleSubmit}>
-//   //       <div className="form-group">
-//   //         <input type="text" className="email-bt" placeholder="Name" name="Name" />
-//   //       </div>
-//   //       <div className="form-group">
-//   //         <input type="text" className="email-bt" placeholder="Email" name="Email" />
-//   //       </div>
-//   //       <div className="form-group">
-//   //         <input type="text" className="email-bt" placeholder="Phone Number" name="PhoneNumber" />
-//   //       </div>
-//   //       <div className="form-group">
-//   //         <textarea className="massage-bt" placeholder="Message" rows="5" id="comment" name="Message"></textarea>
-//   //       </div>
-//   //       <button type="submit" className="send_btn">SEND</button>
-//   //     </form>
-//   //   </div>
-//   // </div>
 //   );
-// }
+// };
 
+// export default Contactus;
 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, TextField, Button } from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: theme.spacing(4),
+  },
+  form: {
+    width: '100%',
+    maxWidth: 400,
+    marginTop: theme.spacing(2),
+  },
+  field: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(2),
+    backgroundColor: '#3f51b5',
+    color: '#fff',
+    '&:hover': {
+      backgroundColor: '#2c387e',
+    },
+  },
+}));
 
-// export default Contactus
-
-import React, { useState } from 'react';
-import { Box, Typography, TextField, Button } from '@mui/material';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Switch from '@mui/material/Switch';
-
-const Contactus = () => {
-  const [checked, setChecked] = useState(true);
-
-  const handleChecked = () => setChecked(!checked);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add form submission logic here
-  };
+function ContactPage() {
+  const classes = useStyles();
 
   return (
-    <Box component="section" py={12} data-aos="fade-up" style={{ backgroundColor: '#DAE2B6', boxShadow: '0 0 10px rgba(0, 100, 0, 0.5)', paddingTop: '50px',marginTop:'50px' }}>
-      <Container>
-        <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
-          <Typography mb={1} sx={{ fontFamily: 'Open Sans , sans-serif', fontSize: '1.5rem', fontWeight: 'bold' }}>
-            Contact <span style={{ color: 'green' }}>Us</span>
-          </Typography>
-        </Grid>
-        <Grid container item xs={12} lg={7} sx={{ mx: 'auto' }}>
-          <Box width="100%" component="form" method="post" autoComplete="off" onSubmit={handleSubmit}>
-            <Box p={3}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} md={6} sx={{ fontFamily: 'Poppins, sans-serif', fontSize: '15px' }}>
-                  <TextField label="First Name" variant="standard" fullWidth required />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                  <TextField label="Last Name" variant="standard" fullWidth required />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField label="Email Address" variant="standard" type="email" fullWidth required />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField label="Your Message" variant="standard" multiline fullWidth rows={6} required />
-                </Grid>
-                <Grid item xs={12} alignItems="center" ml={-1}>
-                  <Switch checked={checked} onChange={handleChecked} />
-                  <Typography variant="button" fontWeight="regular" color="text" ml={-1} sx={{ cursor: 'pointer', userSelect: 'none', fontFamily: 'Poppins, sans-serif' }} onClick={handleChecked}>
-                    &nbsp;&nbsp;I agree to the&nbsp;
-                  </Typography>
-                  <Typography component="a" href="#" variant="button" fontWeight="regular" color="#1F6415">
-                    Terms and Conditions
-                  </Typography>
-                </Grid>
-              </Grid>
-              <Grid container item justifyContent="center" xs={12} my={2}>
-                <Button type="submit" variant="contained" color="success" fullWidth style={{color:"#D2FBA4",backgroundColor: '#1A2902',borderRadius:'15px'}} >
-                  Send Message
-                </Button>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Container>
-    </Box>
+    <div className={classes.container}>
+      <Typography variant="h4" gutterBottom>
+      Contact <span style={{ color: 'green' }}>Us</span>
+      </Typography>
+      <form className={classes.form}>
+        <TextField
+          className={classes.field}
+          fullWidth
+          label="Name"
+          variant="outlined"
+          id="name"
+        />
+        <TextField
+          className={classes.field}
+          fullWidth
+          label="Email"
+          variant="outlined"
+          id="email"
+        />
+        <TextField
+          className={classes.field}
+          fullWidth
+          multiline
+          rows={5}
+          label="Message"
+          variant="outlined"
+          id="message"
+        />
+        <Button
+          className={classes.button}
+          variant="contained"
+          size="large"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
   );
-};
+}
 
-export default Contactus;
+export default ContactPage;
+
+
